c46f6409229542c87cf935db2bc8e616abc874cb
MariliaMoura <62260713+MariliaMoura@users.noreply.github.com>
2020-07-27 15:35:28 -0300

    Semana-3/Aula-9
    Variáveis em JS

---
+++ index.js
@@ -0,0 +1,85 @@
+/*
+EXERCÍCIO 1
+*/
+
+// a = 10
+// b = 10
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+
+/* resultado impresso: 
+10
+10 5
+*/
+
+// a = 10
+// b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+
+// resultado impresso: 10 10 10
+
+// const nome = null
+// let idade = null
+// console.log(typeof(nome, idade))
+
+/* resultado: object 
+ Esse resultado ocorreu porque eu coloquei null nos valores.
+portanto o typeof retorna esse valor.
+ 
+*/
+
+// const nome = prompt('Qual é o seu nome?');
+// let idade = prompt('Qual é a sua idade?');
+// console.log(typeof(nome, idade));
+
+// Resultado impresso:o nome e a idade respondida.
+// Após acrescentar o typeof, o resultado vem como: String
+
+// let nome = prompt('Nome');
+// let idade = prompt('idade')
+
+// console.log("Olá", nome, "você tem", idade, "anos.")
+
+/*
+
+FIM DO EXERCÍCIO 1 
+ 
+*/
+
+// Inicio do Exercício 2
+
+// const nome = prompt('Diz seu nome pra gente');
+// console.log("Olá ", nome);
+
+// const idade = prompt('Quantos anos você tem?');
+// console.log("Você tem", idade, "anos, que legal.")
+
+// const cor = prompt('Qual cor mais te agrada?');
+// console.log(cor, 'realmente é uma cor bem bonita');
+
+// const comida = prompt('Você tem uma comida preferida?');
+// console.log('hmmmmm', comida, 'é mesmo uma delícia.');
+
+// const famoso = prompt('Se pudesse ressucitar um famoso, qual seria?');
+// console.log('uaaaal, adoramos a sua escolha:', famoso);
+
+// FIM DO EXERCÍCIO 2;
+
+// Inicio do Exercício 3
+
+// const frase = "Essas são as minhas comidas preferidas:"
+// console.log(frase)
+// let array = ["Sushi ", "Culinária Árabe ", "Comida Nordestina ", "Taco Mexicano e ", "Sobremesas "]
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+
+// FIM DO EXERCÍCIO 3
